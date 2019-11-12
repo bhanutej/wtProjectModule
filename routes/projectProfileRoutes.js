9c7724a0e146266cb739f16998ebae1480bf86f9
bhanutej <bhanutej.kalla89@gmail.com>
2019-11-12 15:37:56 +0530

    swagger ui for project profile

--- routes/projectProfileRoutes.js
+++ routes/projectProfileRoutes.js
@@ -2,9 +2,86 @@ const checkAuth = require('../middleware/checkAuth');
 const ProjectProfilesController = require('../controllers/projectProfiles');
 
 module.exports = (app) => {
-  app.post('/api/createProjectProfile', checkAuth, ProjectProfilesController.createProjectProfile);
-  app.get('/api/projectProfiles', checkAuth, ProjectProfilesController.projectProfiles);
+  /**
+   * @swagger
+   * /projectProfile:
+   *  get:
+   *    description: Get project profile information
+   *    parameters:
+   *      - name: profileId
+   *        in: body
+   *        required: true
+   */
   app.get('/api/projectProfile', checkAuth, ProjectProfilesController.projectProfileInfo);
-  app.patch('/api/updateProjectProfile/', checkAuth, ProjectProfilesController.updateProjectProfile);
+
+  /**
+   * @swagger
+   * /projectProfiles:
+   *  get:
+   *    description: Get project profiles
+   *    parameters:
+   *      - name: projectId
+   *        in: body
+   *        required: true
+   */
+  app.get('/api/projectProfiles', checkAuth, ProjectProfilesController.projectProfiles);
+
+  /**
+   * @swagger
+   * /createProjectProfile:
+   *  post:
+   *    description: Create project profile page
+   *    parameters:
+   *      - name: projectId
+   *        in: body
+   *        required: true
+   *        type: string
+   *      - name: name
+   *        in: body
+   *        required: true
+   *        type: string
+   *      - name: isDefaultProfile
+   *        in: body
+   *        required: true
+   *        type: boolean
+   */
+  app.post('/api/createProjectProfile', checkAuth, ProjectProfilesController.createProjectProfile);
+
+  /**
+   * @swagger
+   * /updateProjectProfile:
+   *  patch:
+   *    description: Update project profile
+   *    parameters:
+   *      - name: profileId
+   *        in: body
+   *        required: true
+   *        type: string
+   *      - name: name
+   *        in: body
+   *        required: true
+   *        type: string
+   *      - name: structure
+   *        in: body
+   *        required: true
+   *        type: object
+   *      - name: elements
+   *        in: body
+   *        required: true
+   *        type: object
+   */
+  app.patch('/api/updateProjectProfile', checkAuth, ProjectProfilesController.updateProjectProfile);
+
+  /**
+   * @swagger
+   * /deleteProjectProfile:
+   *  delete:
+   *    description: Delete project profile
+   *    parameters:
+   *      - name: profileId
+   *        in: body
+   *        required: true
+   *        type: string
+   */
   app.delete('/api/deleteProjectProfile', checkAuth, ProjectProfilesController.deleteProjectProfile);
 };
