fecc0c209042e1f555446d491df688209f91dd6f
bhanutej <bhanutej.kalla89@gmail.com>
2019-11-12 15:05:25 +0530

    swagger info for project alert

--- routes/projectAlertRoutes.js
+++ routes/projectAlertRoutes.js
@@ -7,16 +7,77 @@ module.exports = (app) => {
    * /projectAlert:
    *  get:
    *    description: Get project alert information
-   *    requestBody:
-   *      required: true
-   *      projectAlertId: qqqqqqqqq
-   *    responses:
-   *      '200':
-   *        description: A successful response
+   *    parameters:
+   *      - name: projectAlertId
+   *        in: body
+   *        required: true
    */
   app.get('/api/projectAlert', checkAuth, ProjectAlertsController.projectAlertInfo);
+
+  /**
+   * @swagger
+   * /projectAlerts:
+   *  get:
+   *    description: Get project alerts
+   *    parameters:
+   *      - name: projectId
+   *        in: body
+   *        required: true
+   */
   app.get('/api/projectAlerts', checkAuth, ProjectAlertsController.projectAlerts);
+
+  /**
+   * @swagger
+   * /createProjectAlert:
+   *  post:
+   *    description: Create project alert
+   *    parameters:
+   *      - name: projectId
+   *        in: body
+   *        required: true
+   *        type: string
+   *      - name: name
+   *        in: body
+   *        required: true
+   *        type: string
+   *      - name: alertInfo
+   *        in: body
+   *        required: true
+   *        type: object
+   */
   app.post('/api/createProjectAlert', checkAuth, ProjectAlertsController.createProjectAlert);
+
+  /**
+   * @swagger
+   * /updateProjectAlert:
+   *  patch:
+   *    description: Update project alert
+   *    parameters:
+   *      - name: projectAlertId
+   *        in: body
+   *        required: true
+   *        type: string
+   *      - name: name
+   *        in: body
+   *        required: true
+   *        type: string
+   *      - name: alertInfo
+   *        in: body
+   *        required: true
+   *        type: object
+   */
   app.patch('/api/updateProjectAlert', checkAuth, ProjectAlertsController.updateProjectAlert);
+
+  /**
+   * @swagger
+   * /deleteProjectAlert:
+   *  delete:
+   *    description: Delete project alert
+   *    parameters:
+   *      - name: projectAlertId
+   *        in: body
+   *        required: true
+   *        type: string
+   */
   app.delete('/api/deleteProjectAlert', checkAuth, ProjectAlertsController.deleteProjectAlert);
 };
