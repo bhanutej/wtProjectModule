3851b0e1073be9e813f000d7e4441f0472e7a389
bhanutej <bhanutej.kalla89@gmail.com>
2019-11-12 15:11:38 +0530

    added swagger ui for project dat scoket module

--- routes/projectDataSocketRoutes.js
+++ routes/projectDataSocketRoutes.js
@@ -2,9 +2,82 @@ const checkAuth = require('../middleware/checkAuth');
 const ProjectDataSocketsController = require('../controllers/projectDataSockets');
 
 module.exports = (app) => {
+  /**
+   * @swagger
+   * /projectDataSocket:
+   *  get:
+   *    description: Get project data socket information
+   *    parameters:
+   *      - name: projectDataSocketId
+   *        in: body
+   *        required: true
+   */
   app.get('/api/projectDataSocket', checkAuth, ProjectDataSocketsController.projectDataSocketInfo);
+
+  /**
+   * @swagger
+   * /projectDataSockets:
+   *  get:
+   *    description: Get project data sockets
+   *    parameters:
+   *      - name: projectId
+   *        in: body
+   *        required: true
+   */
   app.get('/api/projectDataSockets', checkAuth, ProjectDataSocketsController.projectDataSockets);
+
+  /**
+   * @swagger
+   * /createProjectDataSocket:
+   *  post:
+   *    description: Create project alert
+   *    parameters:
+   *      - name: projectId
+   *        in: body
+   *        required: true
+   *        type: string
+   *      - name: name
+   *        in: body
+   *        required: true
+   *        type: string
+   *      - name: socketInfo
+   *        in: body
+   *        required: true
+   *        type: object
+   */
   app.post('/api/createProjectDataSocket', checkAuth, ProjectDataSocketsController.createProjectDataSocket);
+
+  /**
+   * @swagger
+   * /updateProjectDataSocket:
+   *  patch:
+   *    description: Update project alert
+   *    parameters:
+   *      - name: projectDataSocketId
+   *        in: body
+   *        required: true
+   *        type: string
+   *      - name: name
+   *        in: body
+   *        required: true
+   *        type: string
+   *      - name: socketInfo
+   *        in: body
+   *        required: true
+   *        type: object
+   */
   app.patch('/api/updateProjectDataSocket', checkAuth, ProjectDataSocketsController.updateProjectDataSocket);
+
+  /**
+   * @swagger
+   * /deleteProjectDataSocket:
+   *  delete:
+   *    description: Delete project alert
+   *    parameters:
+   *      - name: projectDataSocketId
+   *        in: body
+   *        required: true
+   *        type: string
+   */
   app.delete('/api/deleteProjectDataSocket', checkAuth, ProjectDataSocketsController.deleteProjectDataSocket);
 };
