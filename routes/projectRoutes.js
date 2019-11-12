34e2aad5c18f60a17b77fc3264c5b608ff052f0f
bhanutej <bhanutej.kalla89@gmail.com>
2019-11-12 15:49:06 +0530

    swagger ui for project apis

--- routes/projectRoutes.js
+++ routes/projectRoutes.js
@@ -2,9 +2,74 @@ const checkAuth = require('../middleware/checkAuth');
 const ProjectsController = require('../controllers/projects');
 
 module.exports = (app) => {
+  /**
+   * @swagger
+   * /project:
+   *  get:
+   *    description: Get project information
+   *    parameters:
+   *      - name: projectId
+   *        in: body
+   *        required: true
+   */
+  app.get('/api/project', checkAuth, ProjectsController.projectInfo);
+
+  /**
+   * @swagger
+   * /projects:
+   *  get:
+   *    description: Get projects
+   */
   app.get('/api/projects', checkAuth, ProjectsController.projects);
+
+  /**
+   * @swagger
+   * /createProject:
+   *  post:
+   *    description: Create project
+   *    parameters:
+   *      - name: name
+   *        in: body
+   *        required: true
+   *        type: string
+   *      - name: description
+   *        in: body
+   *        required: true
+   *        type: string
+   */
   app.post('/api/createProject', checkAuth, ProjectsController.createProject);
-  app.get('/api/projects', checkAuth, ProjectsController.projectInfo);
+
+  /**
+   * @swagger
+   * /updateProject:
+   *  patch:
+   *    description: Update project
+   *    parameters:
+   *      - name: projectId
+   *        in: body
+   *        required: true
+   *        type: string
+   *      - name: name
+   *        in: body
+   *        required: true
+   *        type: string
+   *      - name: description
+   *        in: body
+   *        required: true
+   *        type: string
+   */
   app.patch('/api/updateProject', checkAuth, ProjectsController.updateProject);
+
+  /**
+   * @swagger
+   * /deleteProject:
+   *  delete:
+   *    description: Delete project
+   *    parameters:
+   *      - name: projectId
+   *        in: body
+   *        required: true
+   *        type: string
+   */
   app.delete('/api/deleteProject', checkAuth, ProjectsController.deleteProject);
 };
