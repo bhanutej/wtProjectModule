aeab6029846685044d5c352386560d792905649a
bhanutej <bhanutej.kalla89@gmail.com>
2019-11-12 16:04:19 +0530

    swagger ui for user registration and sigin

--- routes/authRoutes.js
+++ routes/authRoutes.js
@@ -8,8 +8,51 @@ module.exports = (app) => {
             res.send(req.user);
         }
     );
-
+    
+    /**
+    * @swagger
+    * /signup:
+    *  post:
+    *    description: User registration
+    *    parameters:
+    *      - name: firstName
+    *        in: body
+    *        required: true
+    *        type: string
+    *      - name: lastName
+    *        in: body
+    *        required: true
+    *        type: string
+    *      - name: email
+    *        in: body
+    *        required: true
+    *        type: string
+    *      - name: password
+    *        in: body
+    *        required: true
+    *        type: string
+    *      - name: phoneNumber
+    *        in: body
+    *        required: true
+    *        type: string
+    */
     app.post('/api/signup', UsersController.userJwtSignup);
+
+    /**
+    * @swagger
+    * /signin:
+    *  post:
+    *    description: User signin
+    *    parameters:
+    *      - name: email
+    *        in: body
+    *        required: true
+    *        type: string
+    *      - name: password
+    *        in: body
+    *        required: true
+    *        type: string
+    */
     app.post('/api/signin', UsersController.userJwtSignin);
     app.get('/api/currentUser', (req, res) => {
         res.send(req.user);
